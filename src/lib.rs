6d1753f4f000c04a17089bec73e3e05043280b6d
Takashi Ogura <t.ogura@gmail.com>
2017-12-26 14:27:48 +0900

    Use num_traits to support f32

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+extern crate num_traits;
+use num_traits::Float;
+
 pub trait Trajectory {
     type Point;
     type Time;
@@ -17,15 +20,19 @@ impl<K, T> TimedPoint<K, T> {
     }
 }
 
-type TimedVec = TimedPoint<f64, Vec<f64>>;
-
-pub struct LinearVectorInterpolator {
-    points: Vec<TimedVec>,
+pub struct LinearVectorInterpolator<T>
+where
+    T: Float,
+{
+    points: Vec<TimedPoint<T, Vec<T>>>,
     dim: usize,
 }
 
-impl LinearVectorInterpolator {
-    pub fn new(points: Vec<TimedVec>) -> Self {
+impl<T> LinearVectorInterpolator<T>
+where
+    T: Float,
+{
+    pub fn new(points: Vec<TimedPoint<T, Vec<T>>>) -> Self {
         Self {
             dim: points[0].point.len(),
             points,
@@ -33,10 +40,13 @@ impl LinearVectorInterpolator {
     }
 }
 
-impl Trajectory for LinearVectorInterpolator {
-    type Point = Vec<f64>;
-    type Time = f64;
-    fn position(&self, t: f64) -> Option<Vec<f64>> {
+impl<T> Trajectory for LinearVectorInterpolator<T>
+where
+    T: Float,
+{
+    type Point = Vec<T>;
+    type Time = T;
+    fn position(&self, t: T) -> Option<Vec<T>> {
         if t < self.points[0].time {
             return None;
         }
@@ -48,21 +58,20 @@ impl Trajectory for LinearVectorInterpolator {
                 let p1 = &self.points[i + 1].point;
                 let t1 = self.points[i + 1].time;
                 for j in 0..p0.len() {
-                    pt[j] += (p1[j] - p0[j]) / (t1 - t0) * (t - t0);
+                    pt[j] = pt[j] + (p1[j] - p0[j]) / (t1 - t0) * (t - t0);
                 }
                 return Some(pt);
             }
         }
         None
     }
-    fn velocity(&self, t: f64) -> Option<Vec<f64>> {
+    fn velocity(&self, t: T) -> Option<Vec<T>> {
         if t < self.points[0].time {
             return None;
         }
         for i in 0..(self.points.len() - 1) {
             if t >= self.points[i].time && t <= self.points[i + 1].time {
-                let mut pt = vec![0.0; self.dim];
-
+                let mut pt = vec![T::zero(); self.dim];
                 let p0 = &self.points[i].point;
                 let t0 = self.points[i].time;
                 let p1 = &self.points[i + 1].point;
@@ -75,24 +84,42 @@ impl Trajectory for LinearVectorInterpolator {
         }
         None
     }
-    fn acceleration(&self, _t: f64) -> Option<Vec<f64>> {
+    fn acceleration(&self, _t: T) -> Option<Vec<T>> {
         None
     }
 }
 
 
-pub struct CSplineVectorInterpolator {
-    points: Vec<TimedVec>,
+pub struct CSplineVectorInterpolator<T>
+where
+    T: Float,
+{
+    points: Vec<TimedPoint<T, Vec<T>>>,
     dim: usize,
-    a: Vec<Vec<f64>>,
-    b: Vec<Vec<f64>>,
-    c: Vec<Vec<f64>>,
-    d: Vec<Vec<f64>>,
+    a: Vec<Vec<T>>,
+    b: Vec<Vec<T>>,
+    c: Vec<Vec<T>>,
+    d: Vec<Vec<T>>,
+}
+
+fn convert<T>(val: f64) -> T
+where
+    T: Float,
+{
+    T::from(val).unwrap()
 }
 
 // from https://en.wikipedia.org/wiki/Spline_(mathematics)
-impl CSplineVectorInterpolator {
-    pub fn new(points: Vec<TimedVec>) -> Self {
+impl<T> CSplineVectorInterpolator<T>
+where
+    T: Float,
+{
+    pub fn new(points: Vec<TimedPoint<T, Vec<T>>>) -> Self {
+        let _0: T = T::zero();
+        let _1: T = T::one();
+        let _2: T = convert(2.0);
+        let _3: T = convert(3.0);
+
         // x_i = points[i].time
         // y_i = points[i].point[j]
         let n = points.len() - 1;
@@ -100,47 +127,47 @@ impl CSplineVectorInterpolator {
         // size of a is n+1
         let a = points.iter().map(|p| p.point.clone()).collect::<Vec<_>>();
         // size of h is n
-        let mut h: Vec<f64> = vec![0.0; n];
+        let mut h: Vec<T> = vec![_0; n];
         for i in 0..n {
             h[i] = points[i + 1].time - points[i].time;
         }
-        let mut alpha = Vec::<Vec<f64>>::new();
+        let mut alpha = Vec::<Vec<T>>::new();
         alpha.reserve(n);
         for i in 0..n {
             // alpha[0] is never used
-            let mut alpha_j = Vec::<f64>::new();
+            let mut alpha_j = Vec::<T>::new();
             for j in 0..dim {
-                let second_elm = if i == 0 {
-                    0.0
+                let second_elm: T = if i == 0 {
+                    _0
                 } else {
-                    3.0 / h[i - 1] * (a[i][j] - a[i - 1][j])
+                    _3 / h[i - 1] * (a[i][j] - a[i - 1][j])
                 };
-                alpha_j.push((3.0 / h[i] * (a[i + 1][j] - a[i][j])) - second_elm);
+                alpha_j.push((_3 / h[i] * (a[i + 1][j] - a[i][j])) - second_elm);
             }
             alpha.push(alpha_j);
         }
-        let mut c = Vec::<Vec<f64>>::new();
-        let mut b = Vec::<Vec<f64>>::new();
-        let mut d = Vec::<Vec<f64>>::new();
-        b.resize(n, vec![0.0; dim]);
-        c.resize(n + 1, vec![0.0; dim]);
-        d.resize(n, vec![0.0; dim]);
-        let mut l = vec![1.0; n + 1];
-        let mut m = vec![0.0; n + 1];
-        let mut z = Vec::<Vec<f64>>::new();
-        z.push(vec![0.0; dim]);
+        let mut c = Vec::<Vec<T>>::new();
+        let mut b = Vec::<Vec<T>>::new();
+        let mut d = Vec::<Vec<T>>::new();
+        b.resize(n, vec![_0; dim]);
+        c.resize(n + 1, vec![_0; dim]);
+        d.resize(n, vec![_0; dim]);
+        let mut l = vec![_1; n + 1];
+        let mut m = vec![_0; n + 1];
+        let mut z = Vec::<Vec<T>>::new();
+        z.push(vec![_0; dim]);
         for i in 1..n {
-            l[i] = 2.0 * (points[i + 1].time - points[i - 1].time) - h[i - 1] * m[i - 1];
+            l[i] = _2 * (points[i + 1].time - points[i - 1].time) - h[i - 1] * m[i - 1];
             m[i] = h[i] / l[i];
-            let mut z_j = Vec::<f64>::new();
+            let mut z_j = Vec::<T>::new();
             z_j.reserve(dim);
             for j in 0..dim {
                 z_j.push((alpha[i][j] - h[i - 1] * z[i - 1][j]) / l[i])
             }
             z.push(z_j);
         }
-        l[n] = 1.0;
-        z.push(vec![0.0; dim]);
+        l[n] = _1;
+        z.push(vec![_0; dim]);
         for j_ in 1..n + 1 {
             let j = n - j_;
             for k in 0..dim {
@@ -148,10 +175,10 @@ impl CSplineVectorInterpolator {
             }
             for k in 0..dim {
                 b[j][k] = (a[j + 1][k] - a[j][k]) / h[j] -
-                    (h[j] * (c[j + 1][k] + 2.0 * c[j][k]) / 3.0);
+                    (h[j] * (c[j + 1][k] + _2 * c[j][k]) / _3);
             }
             for k in 0..dim {
-                d[j][k] = (c[j + 1][k] - c[j][k]) / (3.0 * h[j]);
+                d[j][k] = (c[j + 1][k] - c[j][k]) / (_3 * h[j]);
             }
         }
         Self {
@@ -165,17 +192,20 @@ impl CSplineVectorInterpolator {
     }
 }
 
-impl Trajectory for CSplineVectorInterpolator {
-    type Point = Vec<f64>;
-    type Time = f64;
-    fn position(&self, t: f64) -> Option<Vec<f64>> {
+impl<T> Trajectory for CSplineVectorInterpolator<T>
+where
+    T: Float,
+{
+    type Point = Vec<T>;
+    type Time = T;
+    fn position(&self, t: T) -> Option<Vec<T>> {
         if t < self.points[0].time {
             return None;
         }
         for i in 0..(self.points.len() - 1) {
             if t >= self.points[i].time && t <= self.points[i + 1].time {
-                let mut pt = Vec::<f64>::new();
-                pt.resize(self.dim, 0.0);
+                let mut pt = Vec::<T>::new();
+                pt.resize(self.dim, T::zero());
                 let dx = t - self.points[i].time;
                 for (j, point_iter) in pt.iter_mut().enumerate() {
                     *point_iter = self.a[i][j] + self.b[i][j] * dx + self.c[i][j] * dx * dx +
@@ -186,35 +216,36 @@ impl Trajectory for CSplineVectorInterpolator {
         }
         None
     }
-    fn velocity(&self, t: f64) -> Option<Vec<f64>> {
+    fn velocity(&self, t: T) -> Option<Vec<T>> {
         if t < self.points[0].time {
             return None;
         }
         for i in 0..(self.points.len() - 1) {
             if t >= self.points[i].time && t <= self.points[i + 1].time {
-                let mut pt = Vec::<f64>::new();
-                pt.resize(self.dim, 0.0);
+                let mut pt = Vec::<T>::new();
+                pt.resize(self.dim, T::zero());
                 let dx = t - self.points[i].time;
                 for (j, point_iter) in pt.iter_mut().enumerate() {
-                    *point_iter = self.b[i][j] + 2.0 * self.c[i][j] * dx +
-                        3.0 * self.d[i][j] * dx * dx;
+                    *point_iter = self.b[i][j] + convert::<T>(2.0) * self.c[i][j] * dx +
+                        convert::<T>(3.0) * self.d[i][j] * dx * dx;
                 }
                 return Some(pt);
             }
         }
         None
     }
-    fn acceleration(&self, t: f64) -> Option<Vec<f64>> {
+    fn acceleration(&self, t: T) -> Option<Vec<T>> {
         if t < self.points[0].time {
             return None;
         }
         for i in 0..(self.points.len() - 1) {
             if t >= self.points[i].time && t <= self.points[i + 1].time {
-                let mut pt = Vec::<f64>::new();
-                pt.resize(self.dim, 0.0);
+                let mut pt = Vec::<T>::new();
+                pt.resize(self.dim, T::zero());
                 let dx = t - self.points[i].time;
                 for (j, point_iter) in pt.iter_mut().enumerate() {
-                    *point_iter = 2.0 * self.c[i][j] + 6.0 * self.d[i][j] * dx;
+                    *point_iter = convert::<T>(2.0) * self.c[i][j] +
+                        convert::<T>(6.0) * self.d[i][j] * dx;
                 }
                 return Some(pt);
             }
@@ -227,6 +258,7 @@ impl Trajectory for CSplineVectorInterpolator {
 #[cfg(test)]
 mod tests {
     use super::*;
+    type TimedVec = TimedPoint<f64, Vec<f64>>;
     #[test]
     fn test_linear() {
         let points = vec![
